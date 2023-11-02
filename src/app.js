5c03c9e7f871a4518ce6352b6e2428a962091e2c
Vincent Zerbib <contact@zelabstudio.com>
2023-11-02 17:05:07 +0100

    save work

---
+++ src/app.js
@@ -0,0 +1,41 @@
+// Require the framework and instantiate it
+
+// CommonJs
+const fastify = require('fastify')({
+    logger: true
+});
+const cors = require('@fastify/cors');
+
+fastify.register(cors, { 
+  // put your options here
+  origin: '*'
+})
+
+fastify.register(require('@fastify/mysql'), {
+    connectionString: 'mysql://root:root@127.0.0.1:8889/ynov_app'
+});
+
+// Declare a route
+fastify.get('/', function (request, reply) {
+    reply.send({ hello: 'world' })
+})
+
+
+fastify.get('/api/search', function (req, reply) {
+    console.log(req.query);
+    fastify.mysql.query(
+        'SELECT * FROM Articles WHERE title LIKE ?', [`%${req.query.string}%`],
+        function onResult(err, result) {
+            reply.send(err || result)
+        }
+    )
+})
+
+// Run the server!
+fastify.listen({ port: 3131 }, function (err, address) {
+    if (err) {
+        fastify.log.error(err)
+        process.exit(1)
+    }
+    // Server is now listening on ${address}
+})
